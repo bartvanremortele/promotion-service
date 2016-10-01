ee54be8702edf326e5d1367609a352c69229473a
Nicolás Cornaglia <ncornag@gmail.com>
2016-10-01 20:04:46 +0200

    Test simple promotion (3x2), simple and multiple items in cart, fulfilled, almost fulfilled and no fulfilled.

--- src/test/test.js
+++ src/test/test.js
@@ -17,6 +17,8 @@ const server = base.services.server;
 
 const defaultHeaders = base.config.get('test:defaultHeaders');
 
+const defaultPromoClass = 'default';
+
 // Check the environment
 if (process.env.NODE_ENV !== 'test') {
   console.log('\n[test] THIS ENVIRONMENT IS NOT FOR TEST!\n');
@@ -59,10 +61,48 @@ function callService(options) {
   return server.inject(options);
 }
 
+// Helper to create promotions
+function createPromotion(payload) {
+  Object.assign(payload, {
+    title: 'Promotion',
+    class: defaultPromoClass,
+    active: true,
+    priority: 100
+  });
+  return callService({
+    url: '/services/promotion/v1/promotion.create',
+    payload
+  });
+}
+
+// Helper to evaluate promotions
+function evaluatePromotions(cart) {
+  return callService({
+    url: '/services/promotion/v1/promotion.cartPromotions?cartId=H19PRsec',
+    payload: cart
+  });
+}
+
+// Helper to mock a successful stock:reserve call
+function mockProductList(id, fields) {
+  const ids = id.split(',');
+  nock('http://gateway')
+    .post('/services/catalog/v1/product.list', {
+      id,
+      fields
+    })
+    .reply(200, {
+      ok: true,
+      page: { limit: 10, skip: 0 },
+      data: ids.map(id => ({ id, categories: ['BJeVBrmsV'] }))
+    });
+}
+
 /*
  Promotion Tests
  */
-describe('Promotion', () => {
+
+describe('Promotion CRUDs', () => {
   beforeEach(done => {
     initDB(done);
   });
@@ -71,6 +111,309 @@ describe('Promotion', () => {
   });
 
   it('creates a Promotion', done => {
-    done();
+    const payload = {
+      if: {}
+    };
+    createPromotion(payload)
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        // Expected result:
+        //
+        // {
+        //   ok: true,
+        //   promotion: {
+        //     id: 'SJp7i8TT',
+        //     title: 'Promotion',
+        //     class: 'default',
+        //     active: true,
+        //     priority: 100,
+        //     if: {}
+        //   }
+        // }
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+        expect(response.result.promotion).to.be.an.instanceof(Object);
+        const promotion = response.result.promotion;
+        expect(promotion.id).to.be.a.string();
+        expect(promotion.title).to.be.a.string().and.to.equal(payload.title);
+        expect(promotion.class).to.be.a.string().and.to.equal(payload.class);
+        expect(promotion.active).to.be.a.boolean().and.be.a.boolean();
+        expect(promotion.priority).to.be.a.number().and.to.equal(payload.priority);
+        expect(promotion.if).to.be.an.instanceof(Object);
+        done();
+      });
   });
+});
+
+describe('Calculate promotions', () => {
+  beforeEach(done => {
+    initDB(done);
+  });
+  after(done => {
+    cleanDB(done);
+  });
+
+  it('3x2 fulfilled single item', done => {
+    const productId = 'SksexGRPn4';
+    const quantity = 3;
+    let promoId;
+    const promotion = {
+      if: {
+        product: {
+          id: productId,
+          quantity
+        }
+      }
+    };
+    createPromotion(promotion)
+      .then(creationResponse => {
+        promoId = creationResponse.result.promotion.id;
+        const cart = {
+          items: [
+            {
+              id: '0',
+              productId,
+              quantity,
+              price: 100.00
+            }
+          ]
+        };
+        mockProductList(productId, 'categories');
+        return evaluatePromotions(cart);
+      })
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        base.logger.debug(JSON.stringify(response.result, null, 2));
+
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+
+        const ffps = response.result.fulfilledPromos;
+        expect(ffps).to.be.an.array().and.to.have.length(1);
+        const ffp = ffps[0];
+        expect(ffp.id).to.be.a.string().and.to.equal(promoId);
+        expect(ffp.items).to.be.an.array().and.to.have.length(1);
+        expect(ffp.items[0].itemId).to.be.a.string().and.to.equal('0');
+        expect(ffp.items[0].quantityUsed).to.be.a.number().and.to.equal(quantity);
+
+        const affps = response.result.almostFulfilledPromos;
+        expect(affps).to.be.an.array().and.to.have.length(0);
+        done();
+      });
+  });
+
+  it('3x2 fulfilled multiple items', done => {
+    const productId = 'SksexGRPn4';
+    const quantity = 3;
+    let promoId;
+    const promotion = {
+      if: {
+        product: {
+          id: productId,
+          quantity
+        }
+      }
+    };
+    createPromotion(promotion)
+      .then(creationResponse => {
+        promoId = creationResponse.result.promotion.id;
+        const cart = {
+          items: [
+            {
+              id: '0',
+              productId,
+              quantity: quantity - 1,
+              price: 100.00
+            }, {
+              id: '1',
+              productId,
+              quantity: 1,
+              price: 100.00
+            }
+          ]
+        };
+        mockProductList(productId, 'categories');
+        return evaluatePromotions(cart);
+      })
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        base.logger.debug(JSON.stringify(response.result, null, 2));
+
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+
+        const ffps = response.result.fulfilledPromos;
+        expect(ffps).to.be.an.array().and.to.have.length(1);
+        const ffp = ffps[0];
+        expect(ffp.id).to.be.a.string().and.to.equal(promoId);
+        expect(ffp.items).to.be.an.array().and.to.have.length(2);
+        expect(ffp.items[0].itemId).to.be.a.string().and.to.equal('0');
+        expect(ffp.items[0].quantityUsed).to.be.a.number().and.to.equal(quantity - 1);
+        expect(ffp.items[1].itemId).to.be.a.string().and.to.equal('1');
+        expect(ffp.items[1].quantityUsed).to.be.a.number().and.to.equal(1);
+
+        const affps = response.result.almostFulfilledPromos;
+        expect(affps).to.be.an.array().and.to.have.length(0);
+        done();
+      });
+  });
+
+  it('3x2 almost fulfilled single item', done => {
+    const productId = 'SksexGRPn4';
+    const quantity = 3;
+    let promoId;
+    const promotion = {
+      if: {
+        product: {
+          id: productId,
+          quantity
+        }
+      }
+    };
+    createPromotion(promotion)
+      .then(creationResponse => {
+        promoId = creationResponse.result.promotion.id;
+        const cart = {
+          items: [
+            {
+              id: '0',
+              productId,
+              quantity: quantity - 1,
+              price: 100.00
+            }
+          ]
+        };
+        mockProductList(productId, 'categories');
+        return evaluatePromotions(cart);
+      })
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        base.logger.debug(JSON.stringify(response.result, null, 2));
+
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+
+        const affps = response.result.almostFulfilledPromos;
+        expect(affps).to.be.an.array().and.to.have.length(1);
+        const affp = affps[0];
+        expect(affp.id).to.be.a.string().and.to.equal(promoId);
+
+        expect(affp.data.collectedQuantity).to.be.a.number().and.to.equal(quantity - 1);
+        expect(affp.data.promoQuantity).to.be.a.number().and.to.equal(quantity);
+        expect(affp.data.missingQuantity).to.be.a.number().and.to.equal(1);
+        expect(affp.data.type).to.be.a.string().and.to.equal('PRODUCT');
+        expect(affp.data.code).to.be.a.string().and.to.equal(productId);
+
+        expect(affp.data.items).to.be.an.array().and.to.have.length(1);
+        expect(affp.data.items[0].itemId).to.be.a.string().and.to.equal('0');
+        expect(affp.data.items[0].quantityToUse).to.be.a.number().and.to.equal(quantity - 1);
+
+        const ffps = response.result.fulfilledPromos;
+        expect(ffps).to.be.an.array().and.to.have.length(0);
+
+        done();
+      });
+  });
+
+  it('3x2 almost fulfilled multiple item', done => {
+    const productId = 'SksexGRPn4';
+    const quantity = 3;
+    let promoId;
+    const promotion = {
+      if: {
+        product: {
+          id: productId,
+          quantity
+        }
+      }
+    };
+    createPromotion(promotion)
+      .then(creationResponse => {
+        promoId = creationResponse.result.promotion.id;
+        const cart = {
+          items: [
+            {
+              id: '0',
+              productId,
+              quantity: 1,
+              price: 100.00
+            }, {
+              id: '1',
+              productId,
+              quantity: 1,
+              price: 100.00
+            }
+          ]
+        };
+        mockProductList(productId, 'categories');
+        return evaluatePromotions(cart);
+      })
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        base.logger.debug(JSON.stringify(response.result, null, 2));
+
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+
+        const affps = response.result.almostFulfilledPromos;
+        expect(affps).to.be.an.array().and.to.have.length(1);
+        const affp = affps[0];
+        expect(affp.id).to.be.a.string().and.to.equal(promoId);
+
+        expect(affp.data.collectedQuantity).to.be.a.number().and.to.equal(quantity - 1);
+        expect(affp.data.promoQuantity).to.be.a.number().and.to.equal(quantity);
+        expect(affp.data.missingQuantity).to.be.a.number().and.to.equal(1);
+        expect(affp.data.type).to.be.a.string().and.to.equal('PRODUCT');
+        expect(affp.data.code).to.be.a.string().and.to.equal(productId);
+
+        expect(affp.data.items).to.be.an.array().and.to.have.length(2);
+        expect(affp.data.items[0].itemId).to.be.a.string().and.to.equal('0');
+        expect(affp.data.items[0].quantityToUse).to.be.a.number().and.to.equal(1);
+        expect(affp.data.items[1].itemId).to.be.a.string().and.to.equal('1');
+        expect(affp.data.items[1].quantityToUse).to.be.a.number().and.to.equal(1);
+
+        const ffps = response.result.fulfilledPromos;
+        expect(ffps).to.be.an.array().and.to.have.length(0);
+
+        done();
+      });
+  });
+
+  it('3x2 not filled', done => {
+    const productId = 'SksexGRPn4';
+    const quantity = 3;
+    let promoId;
+    const promotion = {
+      if: {
+        product: {
+          id: productId,
+          quantity
+        }
+      }
+    };
+    createPromotion(promotion)
+      .then(creationResponse => {
+        promoId = creationResponse.result.promotion.id;
+        const cart = {
+          items: [
+            {
+              id: '0',
+              productId,
+              quantity: quantity - 2,
+              price: 100.00
+            }
+          ]
+        };
+        mockProductList(productId, 'categories');
+        return evaluatePromotions(cart);
+      })
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        base.logger.debug(JSON.stringify(response.result, null, 2));
+
+        expect(response.result.ok).to.be.a.boolean().and.to.equal(true);
+
+        expect(response.result.almostFulfilledPromos).to.be.an.array().and.to.have.length(0);
+        expect(response.result.fulfilledPromos).to.be.an.array().and.to.have.length(0);
+
+        done();
+      });
+  });
+
+
 });
